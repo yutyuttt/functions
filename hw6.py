7b1a9f4586353e75813da26165d831250197c92a
yutyuttt <319337790+yutyuttt@users.noreply.github.com>
2026-09-11 08:43:03 +0300

    add solution

--- hw6.py
+++ hw6.py
@@ -1,3 +1,4 @@
+# only works for odd length lists
 def alternate(lst):
     res = []
     for i in range(len(lst) // 2):
@@ -8,3 +9,18 @@ def alternate(lst):
 
 a = [1, 2, 3, 4, 5]
 print(alternate(a))
+
+# takes into account both even and odd length lists
+def alternate2(lst):
+    res = []
+    beg = 0
+    end = len(lst) - 1
+    for i in range(len(lst)):
+        if i % 2 == 0:
+            res.append(lst[beg])
+            beg += 1
+        else:
+            res.append(lst[end])
+            end -= 1
+
+    return res
